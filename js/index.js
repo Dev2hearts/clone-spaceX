554be8368e8e533f1f44262d5d74f2592261554d
dev2hearts <js.park119de@gmail.com>
2023-05-12 16:33:46 +0900

    feat: header 작업 완료

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 AOS.init();
 
-// var burger = $(".menu-trigger");
-
-// burger.each(function (index) {
-//     var $this = $(this);
-
-//     $this.on("click", function (e) {
-//         e.preventDefault();
-//         $(this).toggleClass("active-" + (index + 1));
-//     });
-// });
-
 let burger = document.querySelector(".menu-trigger");
 let sideWrap = document.querySelector(".side-wrap");
 let sideGnb = document.querySelector(".side-gnb");
+let header = document.querySelector(".header");
 burger.addEventListener("click", function () {
     burger.classList.toggle("active-1");
     sideWrap.classList.toggle("side-wrap-active");
     sideGnb.classList.toggle("side-gnb-active");
+});
+sideWrap.addEventListener("click", function () {
+    burger.classList.remove("active-1");
+    sideWrap.classList.remove("side-wrap-active");
+    sideGnb.classList.remove("side-gnb-active");
+});
+window.addEventListener("scroll", function () {
+    burger.classList.remove("active-1");
+    sideWrap.classList.remove("side-wrap-active");
+    sideGnb.classList.remove("side-gnb-active");
+});
+let lastScroll = document.documentElement.scrollTop || 0;
+document.addventListener("scroll", function () {
+    let scrollTop = document.documentElement.scrollTop;
+    if (scrollTop > lastScroll) {
+        header.classList.add("header-active");
+    } else {
+        header.classList.remove("header-active");
+    }
+    lastScroll = scrollTop;
 });
